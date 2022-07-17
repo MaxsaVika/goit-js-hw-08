7283e69b8c5f00050b4b30e5992aa60ab9c3a710
MaxsaVika <viktoria.plotitsyna@gmail.com>
2022-07-17 15:27:13 +0300

    Add Task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const timeFromStorage = localStorage.getItem(STORAGE_KEY);
+
+const timeUpdate = function ({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+  console.log(seconds);
+};
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+player.setCurrentTime(timeFromStorage).then(function () {});
