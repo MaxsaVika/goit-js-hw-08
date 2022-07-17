211974b394096bea074411fd111bcbaa4c601028
MaxsaVika <viktoria.plotitsyna@gmail.com>
2022-07-17 16:26:40 +0300

    Add Task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+onCheckForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  formData.email = input.value;
+  formData.message = textarea.value;
+  console.log(formData);
+
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onCheckForm() {
+  const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+
+    textarea.value = message;
+    input.value = email;
+  }
+}
